7f4bbf1e16b6bcdab058b06a6d9d9efd15a630ac
sumeyyenurozgenc <sumeyyenurozgenc@gmail.com>
2021-01-17 00:04:58 +0300

    16.01.2021 Kodlama.io kampında yapılanlar

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -15,7 +15,7 @@ namespace DegerVeReferansTipler
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
-            sayilar2[0] = 999;
+            sayilar2[0] = 998;
 
             Console.WriteLine(sayilar1[0]);
         }
